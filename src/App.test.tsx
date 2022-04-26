df8af98f433fe24e66a4e69c9b8a914470e6bd96
Thiago Bergenthal de Borba <thiago_borba@terceiros.sicredi.com.br>
2022-04-26 15:39:25 -0300

    feat: Atualiza test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+jest.mock('./utils')
+
 test('renders learn react link', () => {
+
   render(<App />);
 });
