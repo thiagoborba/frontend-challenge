c2db851c531894a6b4a51cad70c3a47861810e95
Thiago Bergenthal de Borba <thiago_borba@terceiros.sicredi.com.br>
2022-04-27 18:23:19 -0300

    feat: atualiza model

--- src/Api/models/index.ts
+++ src/Api/models/index.ts
@@ -98,7 +98,7 @@ export type Film = {
 	opening_crawl:string,
 	director: string,
 	producer: string,
-	release_string: string,
+	release_date: string,
 	characters: string[
 	],
 	planets: string[],
