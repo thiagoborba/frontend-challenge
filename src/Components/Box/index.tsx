ff2c6e44d2f7cdfa690979b817c32a32ef3887f7
Thiago Bergenthal de Borba <thiago_borba@terceiros.sicredi.com.br>
2022-04-27 17:14:55 -0300

    feat: atualiza componentes

--- src/Components/Box/index.tsx
+++ src/Components/Box/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import styles from './styles.module.scss'
+import cn from 'clsx'
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {}
 
-export const Box: React.FC<Props> = ({ children, ...props }) => {
+export const Box: React.FC<Props> = ({ children, className, ...props }) => {
   return (
     <div
-      className={styles.box}
+      className={cn(styles.box, className)}
       {...props}
     >
       { children }
--- src/Components/Layout/index.tsx
+++ src/Components/Layout/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import cn from 'clsx'
 import styles from './styles.module.scss'
 
 export interface Props extends React.HTMLAttributes<HTMLDivElement> {}
 
-export const Layout: React.FC<Props> = ({ children, ...props }) => {
+export const Layout: React.FC<Props> = ({ children, className, ...props }) => {
   return (
     <div
-      className={styles['container']}
+      className={cn(styles['container'], className)}
       {...props}
     >
       { children }
